ae787fd4fadc0116eeb6ededd9e0dab01ee1143c
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-12-05 22:47:26 +0000

    Preparing for better-auth + convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,18 @@
 "use client"
 
-import ThemeToggle from "@/components/global/theme-toggle"
 import { Button } from "@/components/ui/button"
-import { toast } from "@/lib/toast-store"
+import Link from "next/link"
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">Toast Verification</h1>
-      <div className="flex gap-4">
-        <Button onClick={() => toast.normal("This is a normal toast")}>
-          Normal Toast
-        </Button>
-        <Button
-          variant="default"
-          className="bg-green-600 hover:bg-green-700"
-          onClick={() => toast.success("Redirecting you to dashboard")}
-        >
-          Success Toast
-        </Button>
-        <Button
-          variant="destructive"
-          onClick={() => toast.error("Invalid code try again")}
-        >
-          Error Toast
-        </Button>
-      </div>
-      <ThemeToggle />
-    </div>
+    <main>
+      <Link href="/sign-up">
+        <Button>Sign Up</Button>
+      </Link>
+      <Link href="/sign-in">
+        <Button variant="outline">Sign In</Button>
+      </Link>
+    </main>
   )
 }
 
